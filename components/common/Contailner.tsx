2d688c9124e8d0b4b27b9e57f95cff080cc11782
gyeon <rlarlgus6077@hanmail.net>
2026-04-02 22:52:56 +0900

    [feat] 컨테이너 컴포넌트 추가

--- components/common/Contailner.tsx
+++ components/common/Contailner.tsx
@@ -1,8 +1,8 @@
 import {View} from "react-native";
 
-export default function Container({ children }: { children: React.ReactNode }) {
+export default function Container({ children, className }: { children: React.ReactNode; className?: string }) {
   return (
-    <View style={{backgroundColor:"rgb(17,21,26)",flex: 1}}>
+    <View style={{backgroundColor:"rgb(17,21,26)",flex: 1}} className={className}>
       {children}
     </View>
   );

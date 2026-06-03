b30dc01b00f74b77139af3e31228de6f11656476
gyeon <rlarlgus6077@hanmail.net>
2026-06-03 23:00:11 +0900

    [feat] axios인스턴스 생성

---
+++ util/async-request.ts
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import {API_URL} from '@env';
+
+interface RequestProps {
+    data?: any;
+    params?: any;
+    headers?: any;
+    timeout?: number;
+}
+
+interface RequestObj {
+    handleGet: (url: string, options?: RequestProps) => Promise<any>;
+    handlePost: (url: string, options?: RequestProps) => Promise<any>;
+    handlePetch: (url: string, options?: RequestProps) => Promise<any>;
+    handlePut: (url: string, options?: RequestProps) => Promise<any>;
+    handleDelete: (url: string, options?: RequestProps) => Promise<any>;
+}
+
+const axiosInit = axios.create({
+    baseURL: API_URL, // 메인 API URL 설정
+    // baseURL: API_URL, // 메인 API URL 설정
+    timeout: 10000, // Set a timeout for requests (optional)
+    headers: {
+        'Content-Type': 'application/json', // Set default headers (optional)
+        credentials: 'true', 
+    },
+});
+
+const axiosInstance =  () : RequestObj => {
+    /** 조회 요청 */
+    const handleGet = async (url: string, options?: RequestProps)=> {
+        const response = await axiosInit.get(url, {
+            ...options
+        });
+        return response.data;
+    }
+
+    /** 등록 요청 */
+    const handlePost = async (url: string, options?: RequestProps) => {
+        const response = await axiosInit.post(url, options?.data, {
+            ...options
+        });
+        return response.data;
+    }
+
+    /** 일부 수정 요청 */
+    const handlePetch = async (url: string, options?: RequestProps) => {
+        const response = await axiosInit.patch(url, options?.data, {
+            ...options
+        });
+        return response.data;
+    }
+
+    /** 수정 요청 */
+    const handlePut = async (url: string, options?: RequestProps) => {
+        const response = await axiosInit.put(url, options?.data, {
+            ...options
+        });
+        return response.data;
+    }
+
+    /** 삭제 요청 */
+    const handleDelete = async (url: string, options?: RequestProps) => {
+        const response = await axiosInit.delete(url, {
+            ...options
+        });
+        return response.data;
+    }
+
+    return {handleGet, handlePost, handlePetch,handlePut, handleDelete};
+}
+
+export default axiosInstance;

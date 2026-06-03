48d4bb438df11619a2537f936f4a3acba2e1c53c
gyeon <rlarlgus6077@hanmail.net>
2026-06-03 23:00:22 +0900

    [feat]주류 조회 훅 추가

---
+++ hooks/useDrink.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from '../util/async-request';
+
+const { handleGet } = axiosInstance();
+
+export const useGetDrinkList = (type: string) => {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['drinkList', type],
+        queryFn: async () => {
+            const response = await handleGet(`/drink`)
+            if(response.status !== 200) {
+                throw new Error('Failed to fetch drink list');
+            }
+
+            return response;
+        }
+    });
+
+    return { drinkList: data, isLoading, error };
+}
+   
